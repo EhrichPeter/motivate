bc7574272455089f1bf5a9b191f8ea4833a62ca6
Peter Ehrich <pe.ehrich@gmail.com>
2024-03-27 15:12:06 +0100

    Padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center">
       <Header />
 
-      <div className="my-auto p-8">
+      <div className="mt-20 px-8">
         <QuoteCard quote={quote} photos={photos} />
       </div>
     </main>
