1359f8dc3dbb2b881b998e36b60fd4adf356a77c
Peter Ehrich <pe.ehrich@gmail.com>
2024-03-27 11:17:32 +0100

    Make a header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { ModeToggle } from "@/components/ui/mode-toggle";
 import { QuoteIcon } from "lucide-react";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
+import Header from "./_components/header";
 
 export default async function Page() {
   const HOUR = 60 * 60;
@@ -13,23 +14,7 @@ export default async function Page() {
 
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <header className="fixed top-0 left-0 w-full z-10 h-12 border">
-        <div className="flex container h-full items-center justify-between">
-          <div className="flex items-center gap-2">
-            <QuoteIcon size={24} />
-            <h1 className="text-xl font-bold">quote</h1>
-          </div>
-          <div className="flex items-center gap-2">
-            <Link
-              href="https://github.com/EhrichPeter/motivate"
-              target="_blank" // Add target="_blank" to open link in new window
-            >
-              <GitHubLogoIcon height={24} width={24} />
-            </Link>
-            <ModeToggle />
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <div id="content" className="my-auto m-10">
         <div className="text-center">
