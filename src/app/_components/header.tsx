8db7d73247adc02c197cbac638a7959d15ee8576
Peter Ehrich <pe.ehrich@gmail.com>
2024-03-27 11:19:59 +0100

    Remove Daily quote header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import Link from 'next/link';
-import { QuoteIcon } from 'lucide-react';
-import { GitHubLogoIcon } from '@radix-ui/react-icons';
-import { ModeToggle } from '@/components/ui/mode-toggle';
+import React from "react";
+import Link from "next/link";
+import { QuoteIcon } from "lucide-react";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 const Header: React.FC = () => {
-    return (
-        <header className="fixed top-0 left-0 w-full z-10 h-12 border">
-            <div className="flex container h-full items-center justify-between">
-                <div className="flex items-center gap-5">
-                    <Link href="/">
-                        <div className="flex items-center gap-2">
-                            <QuoteIcon size={24} />
-                            <h1 className="text-xl font-bold">quote</h1>
-                        </div>
-                    </Link>
-
-                    <h1>Daily Quote</h1>
-                </div>
-                <div className="flex items-center gap-2">
-                    <Link
-                        href="https://github.com/EhrichPeter/motivate"
-                        target="_blank" // Add target="_blank" to open link in new window
-                    >
-                        <GitHubLogoIcon height={24} width={24} />
-                    </Link>
-                    <ModeToggle />
-                </div>
+  return (
+    <header className="fixed top-0 left-0 w-full z-10 h-12 border">
+      <div className="flex container h-full items-center justify-between">
+        <div className="flex items-center gap-5">
+          <Link href="/">
+            <div className="flex items-center gap-2">
+              <QuoteIcon size={24} />
+              <h1 className="text-lg font-bold">quote</h1>
             </div>
-        </header>
-    );
+          </Link>
+
+          {/* <h1 className="text-sm">Daily Quote</h1> */}
+        </div>
+        <div className="flex items-center gap-2">
+          <Link
+            href="https://github.com/EhrichPeter/motivate"
+            target="_blank"
+          >
+            <GitHubLogoIcon height={24} width={24} />
+          </Link>
+          <ModeToggle />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
