c67af84ba8bb475302cf3e93b31d60fed80edaa4
Peter Ehrich <pe.ehrich@gmail.com>
2024-03-27 14:36:16 +0100

    Add card around quote content

---
+++ src/app/_components/quote_card.tsx
@@ -0,0 +1,49 @@
+import { Bookmark } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { PhotoCarousel } from "./photos";
+import { Quote } from "@/server/quote";
+import { UnsplashPhoto } from "@/server/unsplash";
+import { Separator } from "@/components/ui/separator";
+
+interface QuoteCardProps {
+  quote: Quote;
+  photos: UnsplashPhoto[];
+}
+
+const QuoteCard = (props: QuoteCardProps) => {
+  const { quote, photos } = props;
+
+  return (
+    <Card className="w-[350px] text-center">
+      <CardHeader>
+        <CardTitle>Quote of the day</CardTitle>
+        <CardDescription>Your daily dose of inspiration</CardDescription>
+        <Separator />
+      </CardHeader>
+      <CardContent>
+        <blockquote className="italic text-xl font-bold">
+          &quot;{quote.q}&quot;
+        </blockquote>
+        -<cite>{quote.a}</cite>
+        <div className="flex justify-center items-center pt-5">
+          <PhotoCarousel photos={photos} photoSize={150} />
+        </div>
+      </CardContent>
+      <CardFooter className="flex justify-end">
+        <Button variant="outline">
+          <Bookmark />
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default QuoteCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { getDailyQuote } from "@/server/quote";
 import { getUnsplashPhoto } from "@/server/unsplash";
-import { PhotoCarousel } from "./_components/photos";
 import Header from "./_components/header";
+import QuoteCard from "./_components/quote_card";
 
-export default async function Page() {
+export default async function Home() {
   const HOUR = 60 * 60;
   const quote = await getDailyQuote(HOUR);
   const photos = await getUnsplashPhoto(quote.q, HOUR, 1, 1);
@@ -13,15 +13,7 @@ export default async function Page() {
       <Header />
 
       <div id="content" className="my-auto m-10">
-        <div className="text-center">
-          <blockquote className="italic text-xl font-bold">
-            &quot;{quote.q}&quot;
-          </blockquote>
-          -<cite>{quote.a}</cite>
-        </div>
-        <div className="flex justify-center items-center m-10">
-          <PhotoCarousel photos={photos} photoSize={150} />
-        </div>
+        <QuoteCard quote={quote} photos={photos} />
       </div>
     </main>
   );
