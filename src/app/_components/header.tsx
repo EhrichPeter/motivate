bc08890929c8a22a14d4abab3052190853da3065
Peter Ehrich <pe.ehrich@gmail.com>
2024-03-28 20:45:51 +0100

    Blur effect for header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -14,8 +14,8 @@ const Header = async () => {
   const { user } = (await supabase.auth.getUser()).data;
 
   return (
-    <header className="fixed top-0 left-0 w-full z-10 bg-background">
-      <div className="flex container items-center justify-between pt-4">
+    <header className="fixed top-0 left-0 w-full z-10 bg-background bg-opacity-70 backdrop-blur-sm">
+      <div className="flex container items-center justify-between py-2">
         <Logo />
         <div className="flex items-center gap-3">
           <ModeToggle />
