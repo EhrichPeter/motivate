708e2b9a8c1b0126c32179f2d52648946ea770e9
Peter Ehrich <peter.ehrich@ext.elli.eco>
2024-04-01 11:28:25 +0200

    Add some logs for quote creation

--- src/server/quotes/actions.ts
+++ src/server/quotes/actions.ts
@@ -10,10 +10,15 @@ export async function cronCreateOne(): Promise<void> {
   const quote = await getDailyZenquote();
   const photo = await getUnsplashPhoto(quote.q);
 
-  await supabase.from('quotes').insert({
+  console.log('quote', quote);
+  console.log('photo', photo.description);
+
+  const res = await supabase.from('quotes').insert({
     author: quote.a,
     quote: quote.q,
     picture_link: photo.urls.regular,
-    picture_alt: photo.description,
+    picture_alt: photo.description ?? 'An inspirational photo',
   });
+
+  console.log('res', res);
 }
